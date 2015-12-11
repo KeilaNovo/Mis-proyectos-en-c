48e03906289d75de19bcd5c27282e14dd6c420e5
kheixx <laurana5@gmail.com>
2015-12-11 21:57:40 +0100

    Update ejercicio_5.c

--- ejercicio_5.c
+++ ejercicio_5.c
@@ -10,40 +10,40 @@ int main(int argc, char *argv[]) {
 	switch(numero_mes)
 	{ 
 	case 1:
-		printf("El primer mes es Enero");
+		printf("El primer mes es Enero\n");
 		break;
 	case 2:
-		printf("El segundo mes es Febrero");
+		printf("El segundo mes es Febrero\n");
 		break;
 	case 3:
-		printf("El tercer mes es Marzo");
+		printf("El tercer mes es Marzo\n");
 		break;
 	case 4:
-		printf("El cuarto mes es Abril");
+		printf("El cuarto mes es Abril\n");
 		break;
 	case 5:
-		printf("El quinto mes es Mayo");
+		printf("El quinto mes es Mayo\n");
 		break;
 	case 6:
-		printf("El sexto mes es Junio");
+		printf("El sexto mes es Junio\n");
 		break;
 	case 7:
-		printf("El septimo mes es Julio");
+		printf("El septimo mes es Julio\n");
 		break;
 	case 8:
-		printf("El octavo mes es Agosto");
+		printf("El octavo mes es Agosto\n");
 		break;
 	case 9:
-		printf("El noveno mes es Septiembre");
+		printf("El noveno mes es Septiembre\n");
 		break;
 	case 10:
-		printf("El decimo mes es Octubre");
+		printf("El decimo mes es Octubre\n");
 		break;
 	case 11:
-		printf("El undecimo mes es Noviembre");
+		printf("El undecimo mes es Noviembre\n");
 		break;
 	case 12:
-		printf("El duodecimo mes es Diciembre");
+		printf("El duodecimo mes es Diciembre\n");
 		break;
 	default:
 		printf("Ha introducido un mes incorrecto.");
